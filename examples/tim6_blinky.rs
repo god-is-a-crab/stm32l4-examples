e045cc92ca9c31491bd8090579c3be523b3c2f6e
god-is-a-crab <ni.jingwei.2210@gmail.com>
2025-03-08 18:52:10 +1000

    F103

--- examples/tim6_blinky.rs
+++ examples/tim6_blinky.rs
@@ -4,22 +4,22 @@
 use cortex_m::asm;
 use cortex_m_rt::entry;
 use panic_semihosting as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-use stm32l4::stm32l4x2::{interrupt, Interrupt, Peripherals as DevicePeripherals};
+use stm32f1::stm32f103::{interrupt, Interrupt, Peripherals as DevicePeripherals};
 
 static mut DEVICE_PERIPHERALS: Option<DevicePeripherals> = None;
 
 #[interrupt]
-fn TIM6_DACUNDER() {
+fn TIM2() {
     #[allow(static_mut_refs)]
     let dp = unsafe { DEVICE_PERIPHERALS.as_mut() }.unwrap();
 
-    if dp.TIM6.sr().read().uif().bit_is_set() {
-        if dp.GPIOA.odr().read().odr9().bit_is_clear() {
-            dp.GPIOA.bsrr().write(|w| w.bs9().set_bit());
+    if dp.TIM2.sr().read().uif().bit_is_set() {
+        if dp.GPIOA.odr().read().odr0().bit_is_clear() {
+            dp.GPIOA.bsrr().write(|w| w.bs0().set_bit());
         } else {
-            dp.GPIOA.bsrr().write(|w| w.br9().set_bit());
+            dp.GPIOA.bsrr().write(|w| w.br0().set_bit());
         }
-        dp.TIM6.sr().write(|w| w.uif().clear_bit());
+        dp.TIM2.sr().write(|w| w.uif().clear_bit());
     }
 }
 
@@ -30,26 +30,27 @@ fn main() -> ! {
     let dp = DevicePeripherals::take().unwrap();
 
     // Chapter 6 - Reset and clock control (RCC)
-    dp.RCC.ahb2enr().write(|w| w.gpioaen().set_bit());
-    dp.RCC.apb1enr1().write(|w| w.tim6en().set_bit());
+    dp.RCC.apb2enr().write(|w| w.iopaen().set_bit());
+    dp.RCC.apb1enr().write(|w| w.tim2en().set_bit());
 
     // Chapter 8 - GPIO
-    dp.GPIOA.moder().write(|w| w.moder9().output());
-    dp.GPIOA.otyper().write(|w| w.ot9().push_pull());
-    dp.GPIOA.ospeedr().write(|w| w.ospeedr9().low_speed());
+    dp.GPIOA
+        .crl()
+        .write(|w| w.mode0().output().cnf0().push_pull());
+    dp.GPIOA.bsrr().write(|w| w.bs0().set_bit());
 
     // Chapter 29 - Basic timer TIM6
     // Set 1s interval
-    dp.TIM6.psc().write(|w| w.psc().set(49999)); // 4Mhz / (49999 + 1) = 80 Hz
-    dp.TIM6.arr().write(|w| w.arr().set(80));
+    dp.TIM2.psc().write(|w| w.psc().set(49999)); // 4Mhz / (49999 + 1) = 80 Hz
+    dp.TIM2.arr().write(|w| w.arr().set(80));
 
     // Enable TIM6 update interrupt
-    dp.TIM6.dier().write(|w| w.uie().set_bit());
-    dp.TIM6.cr1().write(|w| w.cen().set_bit());
+    dp.TIM2.dier().write(|w| w.uie().set_bit());
+    dp.TIM2.cr1().write(|w| w.cen().set_bit());
 
     unsafe {
         DEVICE_PERIPHERALS = Some(dp);
-        cortex_m::peripheral::NVIC::unmask(Interrupt::TIM6_DACUNDER);
+        cortex_m::peripheral::NVIC::unmask(Interrupt::TIM2);
     }
 
     loop {
